bce41456369b9ebdd3fe2fbe490961e4a964d8db
rogueware <marcus.lee1@students.mq.edu.au>
2023-05-30 22:55:25 +1000

    Demo package

--- MyClient.java
+++ MyClient.java
@@ -1,105 +1,128 @@
-import java.net.*;  
-import java.io.*;  
+import java.io.*;
+import java.net.*;
 
+public class MyClient {
+   public static void main(String args[])throws Exception{
+       Socket socket = null;
+       socket = new Socket("localhost",50000);
 
-class MyClient {
-//Function for finding index of whitespace
-    public static int ordinalIndexOf(String str, String substr, int n) {
-        int pos = -1;
-        do {
-            pos = str.indexOf(substr, pos + 1);
-        } while (n-- > 0 && pos != -1);
-        return pos;
-    }
-    public static void main(String args[])throws Exception{  
-        Socket s=new Socket("localhost",50000);  
-        BufferedReader din=new BufferedReader(new InputStreamReader(s.getInputStream()));  
-        DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
+       DataOutputStream dout=new DataOutputStream(socket.getOutputStream()); 
+       BufferedReader din = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+       int norecs = 0;
+       int jobID = 0;
+       String count[];
+
+       String reply = "";
+       String mess = "";
+       String str="",str2="",ok="OK\n",auth="AUTH rogue\n",redy="REDY\n",helo="HELO\n";
+
+       dout.write(helo.getBytes());  
+       dout.flush();
+       str2=din.readLine();  
+
+       dout.write(auth.getBytes());  
+       dout.flush();
+       str2=din.readLine();  
+
+       dout.write(redy.getBytes());  
+       dout.flush();
+       str2=din.readLine();  
         
-        String str="",str2="",ok="OK\n",auth="AUTH rogue\n",redy="REDY\n",helo="HELO\n";
-        String largestname="";
-        int largestcore=0;
-        int numberoflargestserver=0;
+       //This while loop continues to run until there are no more jobs
+        while(!reply.equals("NONE")){
+            if(reply.contains("JOBN")) { 
+                
+               String temp = reply;
+               String arrOfStr[] = temp.split(" ",-1);
+               mess = "GETS Avail "+ arrOfStr[4] + " " + arrOfStr[5] + " " + arrOfStr[6] +"\n";
+               System.out.println(mess);
+               dout.write(mess.getBytes());
+               dout.flush();
+               reply = din.readLine();
 
-        int jobID=0;
-        String hold;
+               temp = reply;
+               count = temp.split(" ", -1);
 
-        //Completing initial connection steps like HELO and AUTH
-        dout.write((helo).getBytes());  
-        dout.flush();
-        str2=din.readLine();  
-        System.out.println("Server says: "+str2);  
+                if(Integer.parseInt(count[1])!=0) { // no server readily available
+                    norecs = Integer.parseInt(count[1]);
+                    count = new String[norecs];
 
-        dout.write((auth).getBytes());  
-        dout.flush();
-        str2=din.readLine();  
-        System.out.println("Server says: "+str2);  
+                    dout.write(ok.getBytes());
+                    dout.flush();
 
-        dout.write((redy).getBytes());  
-        dout.flush();
-        str2=din.readLine();  
-        System.out.println("Server says: "+str2);
+                    for(int i = 0; i < norecs; i++){
+                        count[i] = din.readLine();
+                    }
 
-        //Saving JobID
-        jobID = Integer.valueOf(str2.substring(ordinalIndexOf(str2," ",1)+1,ordinalIndexOf(str2," ",2)));
-        //hold = (str2.substring(ordinalIndexOf(str2," ",1)+1,ordinalIndexOf(str2," ",2)));
+                    dout.write(ok.getBytes());
+                    dout.flush();
+                    reply = din.readLine();
 
-        //Retrive server records
-        dout.write(("GETS All\n").getBytes());  
-        dout.flush();
-        str2=din.readLine();  
-        System.out.println("Server says: "+str2);  
+                    String firstServer[] = count[0].split(" ", -1);
+                    
+                    jobID = Integer.parseInt(arrOfStr[2]);
+                    mess = "SCHD" + " " + jobID +" " + firstServer[0] + " " + firstServer[1] + "\n";
+                    dout.write(mess.getBytes());
+                    dout.flush();
+                    reply = din.readLine();
+                }    
 
-        //Compare and save largest
-        while(!str2.equals(".")){
-            dout.write((ok).getBytes());  
-            dout.flush();
-            str2=din.readLine();  
-            System.out.println("Server says: "+str2);  
+               else{
+                    dout.write(ok.getBytes());
+                    dout.flush();
+                    reply = din.readLine();
 
-            dout.write((auth).getBytes());  
-            dout.flush();
-            str2=din.readLine();  
-            System.out.println("Server says: "+str2);  
+                    mess = "GETS Capable "+ arrOfStr[4] + " " + arrOfStr[5] + " " + arrOfStr[6] +"\n";
+                    dout.write(mess.getBytes());
+                    dout.flush();
+                    reply = din.readLine();
 
-            dout.write((redy).getBytes());  
-            dout.flush();
-            str2=din.readLine();  
-            System.out.println("Server says: "+str2);
+                    temp = reply;
+                    count = temp.split(" ", -1);
 
-            jobID = Integer.valueOf(str2.substring(ordinalIndexOf(str2," ",1)+1,ordinalIndexOf(str2," ",2)));
-            //hold = (str2.substring(ordinalIndexOf(str2," ",1)+1,ordinalIndexOf(str2," ",2)));
+                    norecs = Integer.parseInt(count[1]);
+                    count = new String[norecs];
 
-            dout.write(("GETS All\n").getBytes());  
-            dout.flush();
-            str2=din.readLine();  
-            System.out.println("Server says: "+str2);  
+                    dout.write(ok.getBytes());
+                    dout.flush();
 
-            while(!str2.equals(".")){
-                dout.write((ok).getBytes());  
-                dout.flush();
-                str2=din.readLine();  
-                //System.out.println("Server says: "+str2);
-                if(str2.equals(".")) break;
-                //if(){
-                    largestname = str2.substring(0, str2.indexOf(' '));
-                    numberoflargestserver = Integer.valueOf((str2.substring(ordinalIndexOf(str2," ",0)+1,ordinalIndexOf(str2," ",1))));
-                    largestcore = Integer.valueOf(str2.substring(ordinalIndexOf(str2," ",3)+1,ordinalIndexOf(str2," ",4)));
+                    for(int i = 0; i < norecs; i++){
+                        count[i] = din.readLine();
+                    }
+
+                    dout.write(ok.getBytes());
+                    dout.flush();
+                    reply = din.readLine();
+
+                    String firstServer[] = count[0].split(" ", -1);
+                    
+                    jobID = Integer.parseInt(arrOfStr[2]);
+                    mess = "SCHD" + " " + jobID +" " + firstServer[0] + " " + firstServer[1] + "\n";
+                    dout.write(mess.getBytes());
+                    dout.flush();
+                    reply = din.readLine();
+                }
             }
 
-            //System.out.println(jobID+","+largestname+","+numberoflargestserver+","+largestcore);
-
-            while(!str.equals("stop")){  
-                str=br.readLine();
-                dout.write((str+"\n").getBytes());  
-                dout.flush();
-                str2=din.readLine();  
-                System.out.println("Server says: "+str2);  
-            }  
-        
-            dout.close();  
-            s.close();  
+            dout.write(redy.getBytes());
+            dout.flush();
+            reply = din.readLine();
         }
-    }
+       mess = "QUIT\n";
+       dout.write(mess.getBytes());
+       dout.flush();
+       reply = din.readLine();
+
+       dout.close();
+       socket.close();       
+    }   
+
+// public String WriteFlushRead(String mess, doutputStream out, BufferedReader in) {
+//     out.write(mess.getBytes());
+//     out.flush();
+//     return in.readLine();
+// }
+
 }
+        
